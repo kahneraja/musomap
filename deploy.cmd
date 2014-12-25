abf1731a53d30466922701f0f9310ddf12f78b51
Kahne Raja <kahneraja@gmail.com>
2014-12-26 07:16:58 +1100

    updated deployment config.

--- deploy.cmd
+++ deploy.cmd
@@ -102,7 +102,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
+  :: call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
